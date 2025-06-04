9b540459fcd6b83cee36e5ce499fda8caf2176fb
PragyaPathak147 <pathakpragya@gmail.com>
2025-06-04 12:19:32 +0530

    changes

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import numpy as np
 import streamlit as st
 import pickle
 
-#st.title ("Song Recommender")
+st.title ("Song Recommender")
 with open("movies.pkl",'rb') as f:
     movies=pickle.load(f)
 
-print(movies['title'].values())
+option = st.selectbox(
+    "Choose a category:",
+     movies['title'].values()
+)
+
+# Display the selected option
+st.write("You selected:", option)
